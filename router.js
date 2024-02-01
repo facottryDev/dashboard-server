5ebb351d56f02e469500ecc6de5f224367309944
Styles <bcs_2021035@iiitm.ac.in>
2024-02-01 09:44:09 +0530

    revert

--- router.js
+++ router.js
@@ -2,7 +2,7 @@ import { callAuth } from "./lib/middlewares.js";
 import { Router } from "express";
 const router = Router();
 
-// router.use(callAuth);
+router.use(callAuth);
 
 router.get("/", (req, res) => {
   res.status(200).json("HOME");
